ffccedc73effd817c3e0ab0dcd950c82a5adc383
adriDiazz <adriandiazmanzanares9@gmail.com>
2022-03-04 18:41:38 +0100

    portfolio

---
+++ js/mobileMenu.js
@@ -0,0 +1,13 @@
+
+    const nav = document.querySelector('.navbar__logo-mobile');
+    const changer = document.querySelector('.navbarMobile')
+
+    console.log('hola')
+    console.log(changer)
+    nav.addEventListener('click', ()=> {
+        changer.classList.toggle("navbarsee")
+    })
+
+
+
+
